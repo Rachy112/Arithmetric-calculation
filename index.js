b845650d96a8e35fe8236a40d3c22370bf15e12d
Rachy112 <gabrielrachael7@gmail.com>
2024-10-08 05:17:10 -0700

    git push arithmetic

---
+++ index.js
@@ -0,0 +1,82 @@
+alert("This is a Simple Arithmetric by Gabriel Rachael Omowumi")
+
+document.getElementsByTagName("h1")[0].innerHTML = "Building a Simple Arithmetric using Javascript"
+document.getElementsByTagName("h1")[0].className = "my-header"
+
+
+// Addition Operation
+
+var addtext = "Addtition of 50 and 11"
+document.write(addtext)
+document.write( "<br>")
+
+var x = 50;
+
+var y = 11;
+
+document.write(x + y)
+document.write("<br>")
+
+
+// Subtraction Operation
+document.write("<br>")
+var minusText = "Minus 30 from 15"
+document.write(minusText)
+document.write("<br>")
+
+var minusX = 30,
+    minusY = 15;
+
+document.write(minusX - minusY) 
+document.write("<br>")   
+
+// Multiplication Operation
+document.write("<br>")
+var multiplyText = "20 Multiply by 10"
+document.write(multiplyText)
+document.write("<br>")
+
+var multiplyX = 20,
+    multiplyY = 10;
+
+document.write(multiplyX * multiplyY)
+document.write("<br>")
+
+// Modulo operation
+document.write("<br>")
+var moduloText = "The Remainder of 48 divided by 7"
+document.write(moduloText)
+document.write("<br>")
+
+var moduloX = 48,
+    moduloY = 7;
+
+document.write(moduloX % moduloY)
+document.write("<br>")
+
+// Exponential Operation
+
+document.write("<br>")
+var expoText = "2 Exponential 10"
+document.write(expoText)
+document.write("<br>")
+
+var exponentialY = 2,
+    exponentialX = 10;
+
+document.write(exponentialY ** exponentialX)
+document.write("<br>")    
+
+
+// Division Operation
+
+document.write("<br>")
+var divideText = "Divide 100 by 10"
+document.write(divideText)
+document.write("<br>")
+
+var divideX = 100,
+    divideY = 10;
+
+document.write(divideX / divideY)
+document.write("<br>")  
---
+++ readme.md
@@ -0,0 +1,44 @@
+## Table of contents
+
+
+- [Table of contents](#table-of-contents)
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+
+## Overview
+
+### Screenshot
+
+![Screenshot](image.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Javascript
+- CSS 
+- Mobile-first workflow
+
+### What I learned
+
+I'm starting to learn about javascript and how to use HTML and CSS with Javascript. 
+
+
+## Author
+Markup:
+  - Website - [Gabriel Rachael Omowumi](https://www.instagram.rachy_word1.com)
+  - Frontend Mentor - [@rachy112](https://www.frontendmentor.io/profile/rachy112)
+  - Twitter - [@rachyworld](https://www.twitter.com/rachy112)
